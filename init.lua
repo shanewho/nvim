6f7fc9ca5f8852f67bc0ddee3e92c090d9a37a23
Shane <shaneblazek@gmail.com>
2024-12-20 23:24:05 -0600

    Backup

--- init.lua
+++ init.lua
@@ -1,5 +1,6 @@
 vim.g.base46_cache = vim.fn.stdpath "data" .. "/base46/"
 vim.g.mapleader = " "
+vim.opt.showcmd = false
 
 -- bootstrap lazy and all plugins
 local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
@@ -44,40 +45,46 @@ if vim.g.neovide then
   vim.g.neovide_cursor_animate_command_line = false
   vim.g.neovide_scroll_animation_far_lines = 0
   vim.g.neovide_scroll_animation_length = 0.00
-  vim.g.neovide_input_macos_option_key_is_meta = 'both'
+  vim.g.neovide_input_macos_option_key_is_meta = "both"
 
   local function paste()
-      vim.opt.paste = true
-      return '"+P'
+    vim.opt.paste = true
+    return '"+P'
   end
 
-  vim.keymap.set('n', '<D-s>', ':w<CR>') -- Save
-  vim.keymap.set('v', '<D-c>', '"+y') -- Copy
-  --vim.keymap.set('c', '<D-v>', '<C-R>+') -- Paste command mode
-  --vim.keymap.set('n', '<D-v>', '"+P') -- Paste normal mode
-  --vim.keymap.set('v', '<D-v>', '"+P') -- Paste visual mode
-  --vim.keymap.set('i', '<D-v>', '<ESC>l"+Pli') -- Paste insert mode
-
-  vim.keymap.set('n', '<D-v>', paste , { expr = true })
-  vim.keymap.set('v', '<D-v>', paste , { expr = true })
-  vim.keymap.set('i', '<D-v>', function() vim.opt.paste = true; return '<ESC>l"+Pli' end, { expr = true }) -- Paste insert mode
-  vim.keymap.set('c', '<D-v>', function() vim.opt.paste = true; return '<C-R>+' end, { expr = true }) -- Paste command mode
-
-  vim.api.nvim_create_autocmd("InsertLeave", {
-    callback = function()
-        vim.opt.paste = false
-      end,
-  })
-
-  vim.keymap.set('n', '<D-t>', ':enew<CR>', { noremap = true, silent = true }) -- new
-  vim.keymap.set("n", "<D-a>", "ggVG", { desc = "Select All" })
+  vim.keymap.set({ "i", "n" }, "<D-a>", "<Esc>ggVG") -- select all
+  vim.keymap.set({ "i", "n" }, "<D-w>", ":tabclose<CR>") -- close tab
+  vim.keymap.set({ "i", "n" }, "<D-[>", function()
+    vim.cmd "BufferPrevious"
+  end) -- previous tab
+  vim.keymap.set({ "i", "n" }, "<D-]>", function()
+    vim.cmd "BufferNext"
+  end) -- next tab
+  vim.keymap.set("i", "<D-t>", "<C-o>:tabnew<CR><Esc>") -- new tab (insert)
+  vim.keymap.set("n", "<D-t>", ":tabnew<CR>") -- new tab (insert)
+  vim.keymap.set("i", "<D-s>", "<C-o>:w<CR>") -- save (insert)
+  vim.keymap.set("n", "<D-s>", ":w<CR>") -- save (normal)
+  vim.keymap.set("x", "<D-x>", '"+dm0i<Esc>`0') -- cut (include insert hack to fix whichkey issue #518)
+  vim.keymap.set("x", "<D-c>", '"+y') -- copy
+  vim.keymap.set("i", "<D-v>", "<C-r><C-o>+") -- paste (insert)
+  vim.keymap.set("n", "<D-v>", "i<C-r><C-o>+<Esc>l") -- paste (normal)
+  vim.keymap.set("x", "<D-v>", '"+P') -- paste (visual)
+  vim.keymap.set("c", "<D-v>", "<C-r>+") -- paste (command)
+  vim.keymap.set("n", "<D-z>", "u") -- undo
+  vim.keymap.set({ "i", "n" }, "<D-q>", function()
+    vim.cmd "confirm qa"
+  end) -- quit
 end
 
+--Show errors in window
+vim.diagnostic.config { virtual_text = false }
+vim.cmd [[autocmd CursorHold,CursorHoldI * lua vim.diagnostic.open_float(nil, {focus=false})]]
+
 -- Allow clipboard copy paste in neovim
--- vim.api.nvim_set_keymap('', '<D-v>', '+p<CR>', { noremap = true, silent = true})
--- vim.api.nvim_set_keymap('!', '<D-v>', '<C-R>+', { noremap = true, silent = true})
--- vim.api.nvim_set_keymap('t', '<D-v>', '<C-R>+', { noremap = true, silent = true})
--- vim.api.nvim_set_keymap('v', '<D-v>', '<C-R>+', { noremap = true, silent = true})
+--vim.api.nvim_set_keymap('', '<D-v>', '+p<CR>', { noremap = true, silent = true })
+--vim.api.nvim_set_keymap('!', '<D-v>', '<C-R>+', { noremap = true, silent = true })
+--vim.api.nvim_set_keymap('t', '<D-v>', '<C-R>+', { noremap = true, silent = true })
+--vim.api.nvim_set_keymap('v', '<D-v>', '<C-R>+', { noremap = true, silent = true })
 
 --vim.keymap.set('c', 'q', '<C-w><C-q>') -- Paste visual mode
 
@@ -91,24 +98,26 @@ local function my_on_attach(bufnr)
   api.config.mappings.default_on_attach(bufnr)
 
   -- custom mappings
-  vim.keymap.set("n", "C", api.tree.change_root_to_node,        opts("CD"))
+  vim.keymap.set("n", "C", api.tree.change_root_to_node, opts "CD")
+  vim.keymap.set("n", "i", api.node.open.horizontal, opts "Open Horizontal")
+  vim.keymap.set("n", "v", api.node.open.vertical, opts "Open Vertical")
 end
 
 require("nvim-tree").setup {
   on_attach = my_on_attach,
   update_cwd = true,
   filters = {
-    dotfiles = false,  -- Set to true to show hidden files
-    custom = {},      -- You can add any other custom filters here
+    dotfiles = false, -- Set to true to show hidden files
+    custom = {}, -- You can add any other custom filters here
   },
-  update_focused_file = {
-    enable = true,
-    update_cwd = true
-  },
+  -- update_focused_file = {
+  --   enable = true,
+  --   update_cwd = true
+  -- },
 }
 
-local actions = require("telescope.actions")
-require("telescope").setup({
+local actions = require "telescope.actions"
+require("telescope").setup {
   defaults = {
     mappings = {
       i = {
@@ -116,4 +125,4 @@ require("telescope").setup({
       },
     },
   },
-})
+}
