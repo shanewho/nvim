210d8b10df82c88c39c353cd4256b65d337ae74c
Shane <shaneblazek@gmail.com>
2025-04-03 20:28:53 -0500

    refactor: Update DAP configuration for JavaScript debugging

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -4,12 +4,11 @@ return {
   dependencies = {
     "nvim-neotest/nvim-nio",
     "rcarriga/nvim-dap-ui",
-    "mxsdev/nvim-dap-vscode-js",
     { -- build debugger from source
       "microsoft/vscode-js-debug",
-      version = "1.x",
       build = "npm i && npm run compile vsDebugServerBundle && mv dist out",
     },
+    -- AAAA=https://github.com/microsoft/vscode-js-debug/archive/refs/tags/v1.97.1.tar.gz
   },
   keys = {
     -- normal mode is default
@@ -45,9 +44,29 @@ return {
     },
   },
   config = function()
-    require("dap-vscode-js").setup {
-      debugger_path = vim.fn.stdpath "data" .. "/lazy/vscode-js-debug",
-      adapters = { "pwa-node", "pwa-chrome", "pwa-msedge", "node-terminal", "pwa-extensionHost" },
+    -- require("dap-vscode-js").setup {
+    --   debugger_path = vim.fn.stdpath "data" .. "/lazy/vscode-js-debug",
+    --   adapters = { "pwa-node", "pwa-chrome", "pwa-msedge", "node-terminal", "pwa-extensionHost" },
+    -- }
+
+    local function get_pkg_path(pkg, path)
+      pcall(require, "mason")
+      local root = vim.env.MASON or (vim.fn.stdpath "data" .. "/mason")
+      path = path or ""
+      local ret = root .. "/packages/" .. pkg .. "/" .. path
+      return ret
+    end
+    require("dap").adapters["pwa-node"] = {
+      type = "server",
+      host = "localhost",
+      port = "9231",
+      executable = {
+        command = "node",
+        args = {
+          "/Users/shane/Downloads/js-debug/src/dapDebugServer.js",
+          "9231",
+        },
+      },
     }
 
     local js_based_languages = {
@@ -66,28 +85,6 @@ return {
         -- `--inspect` for longrunning tasks or `--inspect-brk` for short tasks
         -- npm script -> `node --inspect-brk ./node_modules/.bin/vite dev`
         {
-          -- use nvim-dap-vscode-js's pwa-node debug adapter
-          type = "pwa-node",
-          -- attach to an already running node process with --inspect flag
-          -- default port: 9222
-          request = "attach",
-          -- allows us to pick the process using a picker
-          processId = require("dap.utils").pick_process,
-          -- name of the debug action you have to select for this config
-          name = "Attach debugger to existing `node --inspect` process",
-          -- for compiled languages like TypeScript or Svelte.js
-          sourceMaps = true,
-          -- resolve source maps in nested locations while ignoring node_modules
-          resolveSourceMapLocations = {
-            "${workspaceFolder}/**",
-            "!**/node_modules/**",
-          },
-          -- path to src in vite based projects (and most other projects as well)
-          cwd = "${workspaceFolder}/src",
-          -- we don't want to debug code inside node_modules, so skip it!
-          skipFiles = { "${workspaceFolder}/node_modules/**/*.js" },
-        },
-        {
           type = "pwa-chrome",
           name = "Launch Chrome to debug client",
           request = "launch",
@@ -98,6 +95,18 @@ return {
           webRoot = "${workspaceFolder}/src",
           -- skip files from vite's hmr
           skipFiles = { "**/node_modules/**/*", "**/@vite/*", "**/src/client/*", "**/src/*" },
+        },
+        {
+          name = "Next.js: debug server-side",
+          type = "pwa-node",
+          request = "attach",
+          port = 9230,
+          skipFiles = { "<node_internals>/**", "node_modules/**" },
+          cwd = "${workspaceFolder}",
+          sourceMapPathOverrides = {
+            "webpack-internal:///(rsc)/*",
+            "apps/my-app/*",
+          },
         },
         -- only if language is javascript, offer this debug action
         language == "javascript"
